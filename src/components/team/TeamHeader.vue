e2da5c8d4031ee0f8253ccf950303b80b7c77652
Yuly <yuleydiarriagamondragon3108@gmail.com>
2021-12-16 09:52:01 -0600

    Actualizaciones

--- src/components/team/TeamHeader.vue
+++ src/components/team/TeamHeader.vue
@@ -3,10 +3,10 @@
         <v-row align="center" class="py-0 my-0">
             <v-col md="12" sm="12" cols="12" class="py-0 my-0">
                 <p class="google-font mb-0" style="font-weight: 350;font-size:200%"><b>
-                    <span style="color: #1a73e8;">Our</span> Team </b> 
+                    <span style="color: #1a73e8;"></span> Team </b> 
                 </p>
-                <p class="google-font mt-0 hidden-sm-and-down">Google is known all around the world. Everyone is 'googling', checking on 'maps' and communicating in 'gmail'. For simple users, they are services that just works, but not for us. Developers see much more: APIs, scalability issues, complex technology stacks. And that is what GDG is about.</p>
-                <p class="google-font mt-0 ">Our goal is to organize space to connect the best industry experts with Indian audience to boost development of IT. And Our Core Team is:</p>
+                <p class="google-font mt-0 hidden-sm-and-down">El equipo de reality tiene informaciòn sobre la realidad virtual y la realidad aumentada.</p>
+                <p class="google-font mt-0 ">En esta app web contiene informaciòn acerca de aplicaciones ya creadas sobre la realidad virtual y la realidad aumentada, cuales son las app que puedes visualizar de manera gratuita sobre cada una de estas realidades asi mismo cual es su diferencia entre ambas.</p>
                 
             </v-col>
         </v-row>
